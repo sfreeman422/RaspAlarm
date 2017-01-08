661bb86adf4377538aa87544ccf0b0eb3b1f315f
Steven Freeman <sfreeman422@gmail.com>
2017-01-07 19:27:56 -0500

    Framework for main component complete

---
+++ app/components/Main.js
@@ -0,0 +1,58 @@
+var React = require('react');
+
+//Require the children
+var Clock = require("./Children/Clock.js");
+var Weather = require("./Children/Weather.js");
+
+//Require the helper functions
+var helpers = require('./utils/helpers.js');
+
+var Main = React.createClass({
+	getInitialState: function(){
+		return{
+			time: undefined,
+			date: undefined,
+			weatherToday: undefined,
+			weatherHourOne: undefined, 
+			weatherHourTwo: undefined,
+			weatherHourThree: undefined,
+			weatherHourFour: undefined,
+			weatherHourFive: undefined
+		}
+	},
+	_setInfo: function(time, date, weatherToday, weatherOne, weatherTwo, weatherThree, weatherFour, weatherFive){
+		this.setState({
+			time: time,
+			date: date,
+			weatherToday: weatherToday,
+			weatherHourOne: weatherOne,
+			weatherHourTwo: weatherTwo,
+			weatherHourThree: weatherThree,
+			weatherHourFour: weatherFour,
+			weatherHourFive: weatherFive
+		})
+	},
+	componentDidUpdate: function(prevProps, prevState){
+		if(prevState != this.state){
+			console.log("Something has changed...");
+
+			//Code will go here to get the time and other fun stuff. 
+		}
+	},
+	componentDidMount: function(){
+		//Code to get time, date and weather data. 
+	},
+	render: function(){
+		return(
+			<div className="container">
+				<div className="row">
+					<Clock time={this.state.time}/>
+				</div>
+				<div className="row">
+					<Date date={this.state.date}/>
+				</div>
+			</div>)
+	}
+});
+
+module.exports = Main; 
