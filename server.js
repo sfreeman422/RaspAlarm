6805abdf1b805eed858a1cde28b2420096affdb3
sfreeman422 <sfreeman422@gmail.com>
2017-02-06 21:10:53 -0500

    Added Mongoose to the stack. Need to create a DB that includes all of the alarms for the page

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
 var PORT = process.env.PORT || 3000; 
+mongoose.connect("mongodb://localhost:27017", function(){
+	console.log("Connected to MongoDB on port 27017.");
+});
 
 app.use(logger('dev'));
 app.use(express.static('./public'));
