3ce7f016ddff712f009109c1c06ba5b5d7e7cdd3
sfreeman422 <sfreeman422@gmail.com>
2017-02-12 22:16:58 -0500

    Clock on alarmManager increments via onClick events. Working to resolve weird issues surrounding double digits and how they cannot be state unless they are strings

--- app/components/AlarmManager.js
+++ app/components/AlarmManager.js
@@ -1,17 +1,61 @@
 var React = require('react');
-
+var ClockPicker = require('react-clockpicker')
 var AlarmManager = React.createClass({
+	getInitialState: function(){
+		return{
+			hour: 1,
+			minute: 30,
+			ampm: "am"
+		}
+	},
+	_incrementHour: function(){
+		if(this.state.hour == 12){
+			this.setState({
+				hour: 1
+			});
+		}
+		else{
+			this.setState({
+				hour: this.state.hour+1
+			});
+		}
+	},
+	_incrementMinute: function(){
+		if(this.state.minute == 55){
+			this.setState({
+				minute: 0
+			});
+		}
+		else{
+			this.setState({
+				minute: this.state.minute+5
+			})
+		}
+	},
+	_changeAMPM: function(){
+		if(this.state.ampm == "am"){
+			this.setState({
+				ampm: "pm"
+			});
+		}
+		else{
+			this.setState({
+				ampm:"am"
+			})
+		}
+	},
 	render: function(){
 		return(
-		<div className="col-xs-12 verticalCenter" id="alarmManager">
+		<div className="container verticalCenter" id="alarmManager">
 			<div className="row">
 				<div className="col-xs-12" id="timeSet">
-					<h1>00:00am</h1>
+					<h1 id="unselectable" onClick={this._incrementHour}>{this.state.hour}</h1><h1 id="unselectable">:</h1><h1 id="unselectable" onClick={this._incrementMinute}>{this.state.minute}</h1><h1 id="unselectable" onClick={this._changeAMPM}>{this.state.ampm}</h1>
 				</div>
 			</div>
 			<div className="row">
 				<div className="col-xs-12" id="daysOfWeek">
 					<h3>Which days would you like to set this alarm for?</h3>
+					<p>Monday    Tuesday    Wednesday   Thursday   Friday   Saturday   Sunday</p>
 				</div>
 			</div>
 		</div>
