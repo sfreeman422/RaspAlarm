59def7728476b9f92e0e8838fe702013cadaa4bb
Steven Freeman <sfreeman422@gmail.com>
2018-02-04 14:05:32 -0500

    Adjusted the Alarms model to includ a oneTimeUse boolean, this will allow us to have alarms that are only triggered one time, then are removed

--- app/models/Alarms.js
+++ app/models/Alarms.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const alarmSchema = mongoose.Schema({
   time: String,
   dayOfWeek: Array,
+  oneTimeUse: Boolean,
 });
 
 module.exports = mongoose.model('Alarm', alarmSchema);
