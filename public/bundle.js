b8c60f4902408c500d9c46a5036f787dd3aee01a
Steven Freeman <sfreeman422@gmail.com>
2018-02-11 13:44:29 -0500

    Adjusted back to 5 for better formatting

--- public/bundle.js
+++ public/bundle.js
@@ -27909,7 +27909,7 @@
 	          }).then(function (json) {
 	            var weatherArr = [];
 	            // Builds out an array to list weather information.
-	            for (var i = 0; i < 6; i += 1) {
+	            for (var i = 0; i < 5; i += 1) {
 	              weatherArr.push({
 	                condition: json.hourly_forecast[i].condition,
 	                time: json.hourly_forecast[i].FCTTIME.civil,
