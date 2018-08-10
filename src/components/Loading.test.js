63e0e01a7179f15a7f8b721be8709a77be0016cb
Steven Freeman <sfreeman422@gmail.com>
2018-08-10 08:41:37 -0400

    Cleaned up loading

--- src/components/Loading.test.js
+++ src/components/Loading.test.js
@@ -2,7 +2,6 @@ import 'jest';
 import React from 'react';
 import * as Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import configureStore from 'redux-mock-store';
 import * as mocks from '../constants/mocks';
 import { Loading } from './Loading';
 
@@ -10,11 +9,8 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('Loading', () => {
   let loading;
-  let store;
-  const mockStore = configureStore();
 
   it('should initialize properly without an error', () => {
-    store = mockStore(mocks.mockStore);
     loading = Enzyme.shallow(<Loading store={mocks.mockStore} loadingMessage="Test" />);
     expect(loading).toBeDefined();
     expect(loading.state()).toBeDefined();
@@ -22,7 +18,6 @@ describe('Loading', () => {
   });
 
   it('should initialize properly with an error', () => {
-    store = mockStore(mocks.mockStoreWithError);
     loading = Enzyme.shallow(<Loading store={mocks.mockStoreWithError} loadingMessage="Failure" />);
     const loadingProps = loading.instance().props.store;
     expect(loadingProps.locationError).toBe('Test Error');
