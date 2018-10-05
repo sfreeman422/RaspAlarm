2ac02d99acdc77879f272258d308e9d56eb85c00
Steven Freeman <sfreeman422@gmail.com>
2018-10-05 08:16:00 -0400

    Added support for padded zeroes

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const mapStateToProps = state => ({ time: state.time, blinkTime: state.blinkTime });
+const mapStateToProps = state => ({ time: state.time, blinkTime: state.blinkTime, showPadded: state.showPadded });
 
 class ConnectedClock extends React.Component {
   constructor(props) {
