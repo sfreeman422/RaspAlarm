49c7848a2b1a23076cf5d4e5d1b73fee33a8743e
Steven Freeman <sfreeman422@gmail.com>
2018-12-31 13:05:37 -0500

    Fixed issue with weatherIcons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable class-methods-use-this, react/jsx-filename-extension */
-import React from 'react';
-import moment from 'moment';
-import fetch from 'isomorphic-fetch';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import * as actions from './actions/actions';
-import Clock from './components/Clock';
-import Today from './components/Today';
-import Weather from './components/Weather';
-import Alarm from './components/Alarm';
-import Loading from './components/Loading';
-import * as weatherIcons from './components/weatherIcons.json';
+import React from "react";
+import moment from "moment";
+import fetch from "isomorphic-fetch";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import * as actions from "./actions/actions";
+import Clock from "./components/Clock";
+import Today from "./components/Today";
+import Weather from "./components/Weather";
+import Alarm from "./components/Alarm";
+import Loading from "./components/Loading";
+import weatherIcons from "./components/weatherIcons";
 
-const config = require('./private/config.json');
+const config = require("./private/config.json");
 
 const mapDispatchToProps = dispatch => ({
   setTime: time => dispatch(actions.setTime(time)),
@@ -27,8 +27,9 @@ const mapDispatchToProps = dispatch => ({
   reportError: error => dispatch(actions.reportError(error)),
   clearError: () => dispatch(actions.clearError()),
   addLocation: location => dispatch(actions.addLocation(location)),
-  setLastTemperature: temperature => dispatch(actions.setLastTemperature(temperature)),
-  setInitialized: initialized => dispatch(actions.setInitialized(initialized)),
+  setLastTemperature: temperature =>
+    dispatch(actions.setLastTemperature(temperature)),
+  setInitialized: initialized => dispatch(actions.setInitialized(initialized))
 });
 
 const mapStateToProps = state => ({
@@ -41,7 +42,7 @@ const mapStateToProps = state => ({
   weatherArr: state.weatherArr,
   coloredIcons: state.coloredIcons,
   date: state.date,
-  initialized: state.initialized,
+  initialized: state.initialized
 });
 
 const RETRY_INTERVAL = 5000;
@@ -81,9 +82,9 @@ class ConnectedMain extends React.Component {
   }
 
   setTime() {
-    this.props.setTime(moment().format('hh:mma'));
-    this.props.setDate(moment().format('MMMM Do YYYY'));
-    this.props.setToday(moment().format('dddd'));
+    this.props.setTime(moment().format("hh:mma"));
+    this.props.setDate(moment().format("MMMM Do YYYY"));
+    this.props.setToday(moment().format("dddd"));
     if (this.props.sunrise && this.props.sunset) {
       this.determineNightState();
     }
@@ -92,13 +93,16 @@ class ConnectedMain extends React.Component {
   getUserCoordinates() {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve({ lat: position.coords.latitude, long: position.coords.longitude });
+        position => {
+          resolve({
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          });
         },
-        (error) => {
+        error => {
           reject(new Error(`Geolocation failed! \n ${error.message}`));
         },
-        { timeout: 30000 },
+        { timeout: 30000 }
       );
     });
   }
@@ -107,49 +111,73 @@ class ConnectedMain extends React.Component {
     if (this.props.weatherArr && this.props.weatherArr.length > 0) {
       this.props.setLastTemperature(this.props.weatherArr[0].temp.english.raw);
     }
-    return fetch(`https://api.wunderground.com/api/${config.wunderground}/hourly/q/${this.props.userCoords.lat},${this.props.userCoords.long}.json`)
+    return fetch(
+      `https://api.wunderground.com/api/${config.wunderground}/hourly/q/${
+        this.props.userCoords.lat
+      },${this.props.userCoords.long}.json`
+    )
       .then(response => response.json())
       .then(json => this.generateWeatherState(json.hourly_forecast))
-      .catch((err) => {
+      .catch(err => {
         throw new Error(`Weather retrieval failed! \n ${err.message}`);
       });
   }
 
   getSunData() {
-    return fetch(`https://api.wunderground.com/api/${config.wunderground}/astronomy/q/${this.props.userCoords.lat},${this.props.userCoords.long}.json`)
+    return fetch(
+      `https://api.wunderground.com/api/${config.wunderground}/astronomy/q/${
+        this.props.userCoords.lat
+      },${this.props.userCoords.long}.json`
+    )
       .then(response => response.json())
       .then(sundata => ({
-        sunrise: moment(`0${sundata.sun_phase.sunrise.hour}:${sundata.sun_phase.sunrise.minute}am`, 'hh:mm:a'),
-        sunset: moment(`0${sundata.sun_phase.sunset.hour - 12}:${sundata.sun_phase.sunset.minute}pm`, 'hh:mm:a'),
+        sunrise: moment(
+          `0${sundata.sun_phase.sunrise.hour}:${
+            sundata.sun_phase.sunrise.minute
+          }am`,
+          "hh:mm:a"
+        ),
+        sunset: moment(
+          `0${sundata.sun_phase.sunset.hour - 12}:${
+            sundata.sun_phase.sunset.minute
+          }pm`,
+          "hh:mm:a"
+        )
       }))
-      .catch((err) => {
+      .catch(err => {
         throw new Error(`Sunrise/sunset retrieval failed! \n ${err.message}`);
       });
   }
 
   getUserCity(locationObject) {
-    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${locationObject.lat},${locationObject.long}&sensor=true&key=${config.google_geocode}`)
+    return fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
+        locationObject.lat
+      },${locationObject.long}&sensor=true&key=${config.google_geocode}`
+    )
       .then(response => response.json())
-      .then((geoloc) => {
+      .then(geoloc => {
         if (geoloc.error_message) {
           throw new Error(geoloc.error_message);
         }
-        return `${geoloc.results[0].address_components[2].short_name}, ${geoloc.results[0].address_components[4].short_name}`;
+        return `${geoloc.results[0].address_components[2].short_name}, ${
+          geoloc.results[0].address_components[4].short_name
+        }`;
       })
-      .catch((err) => {
+      .catch(err => {
         throw new Error(`Location Refinement Failed! \n Unknown error: ${err}`);
       });
   }
 
   setBrightness(isNight) {
-    fetch('/brightness', {
-      method: 'POST',
+    fetch("/brightness", {
+      method: "POST",
       headers: new Headers({
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json"
       }),
       body: JSON.stringify({
-        isNight,
-      }),
+        isNight
+      })
     })
       .then(res => res.json())
       .then(resp => console.log(resp))
@@ -162,12 +190,15 @@ class ConnectedMain extends React.Component {
       if (this.props.date !== prevProps.date && this.props.initialized) {
         const sunData = await this.getSunData();
         this.props.setSunData(sunData);
-      } else if (this.props.time !== prevProps.time && moment().format('mm') === '00') {
+      } else if (
+        this.props.time !== prevProps.time &&
+        moment().format("mm") === "00"
+      ) {
         const weather = await this.getWeather();
         this.props.setWeather(weather);
       }
     } catch (err) {
-      console.error('Error on runUpdate - ', err.message);
+      console.error("Error on runUpdate - ", err.message);
       this.errorTimeout = setTimeout(this.runUpdate, RETRY_INTERVAL);
       this.props.reportError(err.message);
     }
@@ -177,7 +208,7 @@ class ConnectedMain extends React.Component {
     let weather = weatherArr;
 
     const firstWeatherHour = parseFloat(weather[0].FCTTIME.hour, 10);
-    if (firstWeatherHour === parseFloat(moment().format('H'), 10)) {
+    if (firstWeatherHour === parseFloat(moment().format("H"), 10)) {
       weather = weather.slice(1, 6);
     }
 
@@ -189,17 +220,17 @@ class ConnectedMain extends React.Component {
         temp: {
           english: {
             raw: parseFloat(weather[i].temp.english, 10),
-            display: `${weather[i].temp.english}F`,
+            display: `${weather[i].temp.english}F`
           },
           metric: {
             raw: parseFloat(weather[i].temp.metric, 10),
-            display: `${weather[i].temp.metric}C`,
-          },
+            display: `${weather[i].temp.metric}C`
+          }
         },
         icon: this.determineWeatherIcon(
           weather[i].icon,
-          weather[i].FCTTIME.civil,
-        ),
+          weather[i].FCTTIME.civil
+        )
       });
     }
     return weatherState;
@@ -208,46 +239,57 @@ class ConnectedMain extends React.Component {
   async initializeApp() {
     try {
       this.props.clearError();
-      this.props.setLoadingStatus('Getting Location...');
+      this.props.setLoadingStatus("Getting Location...");
       const userCoordinates = await this.getUserCoordinates();
-      this.props.setLoadingStatus('Refining Location...');
+      this.props.setLoadingStatus("Refining Location...");
       this.props.setUserCoords(userCoordinates);
       const userCity = await this.getUserCity(userCoordinates);
-      this.props.setLoadingStatus('Getting Solar Information...');
+      this.props.setLoadingStatus("Getting Solar Information...");
       this.props.setUserLoc(userCity);
       const sunData = await this.getSunData();
       this.props.setSunData(sunData);
-      this.props.setLoadingStatus('Getting weather...');
+      this.props.setLoadingStatus("Getting weather...");
       const weather = await this.getWeather();
       this.props.setWeather(weather);
       this.props.setInitialized(true);
     } catch (err) {
-      console.error('Error on intiailizeApp - ', err.message);
+      console.error("Error on intiailizeApp - ", err.message);
       this.props.reportError(err.message);
       this.errorTimeout = setTimeout(this.initializeApp, RETRY_INTERVAL);
     }
   }
 
   determineNightState() {
-    const sunrise = moment(this.props.sunrise, 'hh:mm:a');
-    const sunset = moment(this.props.sunset, 'hh:mm:a');
+    const sunrise = moment(this.props.sunrise, "hh:mm:a");
+    const sunset = moment(this.props.sunset, "hh:mm:a");
     const currentTime = moment();
-    if ((currentTime.isAfter(sunset) || currentTime.isBefore(sunrise)) && !this.props.isNight) {
+    if (
+      (currentTime.isAfter(sunset) || currentTime.isBefore(sunrise)) &&
+      !this.props.isNight
+    ) {
       this.props.setNight(true);
       this.setBrightness(true);
-    } else if ((currentTime.isBefore(sunset) && currentTime.isAfter(sunrise)) && (this.props.isNight || this.props.isNight === undefined)) {
+    } else if (
+      currentTime.isBefore(sunset) &&
+      currentTime.isAfter(sunrise) &&
+      (this.props.isNight || this.props.isNight === undefined)
+    ) {
       this.props.setNight(false);
       this.setBrightness(false);
     }
   }
 
   determineWeatherIcon(weatherState, hour) {
-    const sunrise = moment(this.props.sunrise, 'hh:mm:a');
-    const sunset = moment(this.props.sunset, 'hh:mm:a');
-    const currentHour = moment(hour, 'hh:mm:a');
+    const sunrise = moment(this.props.sunrise, "hh:mm:a");
+    const sunset = moment(this.props.sunset, "hh:mm:a");
+    const currentHour = moment(hour, "hh:mm:a");
     if (currentHour.isBefore(sunrise) || currentHour.isAfter(sunset)) {
       return weatherIcons[weatherState].night;
     }
+    console.log(weatherIcons);
+    console.log(weatherState);
+    console.log(weatherIcons[weatherState]);
+    console.log(weatherIcons[weatherState].day);
     return weatherIcons[weatherState].day;
   }
 
@@ -256,8 +298,11 @@ class ConnectedMain extends React.Component {
       <div className="container">
         <Clock />
         <Today />
-        {this.props.weatherArr && this.props.weatherArr.length > 0 ?
-          <Weather /> : <Loading />}
+        {this.props.weatherArr && this.props.weatherArr.length > 0 ? (
+          <Weather />
+        ) : (
+          <Loading />
+        )}
         <Alarm />
       </div>
     );
@@ -286,17 +331,20 @@ ConnectedMain.propTypes = {
   isNight: PropTypes.bool,
   userCoords: PropTypes.shape({
     lat: PropTypes.number.isRequired,
-    long: PropTypes.number.isRequired,
+    long: PropTypes.number.isRequired
   }).isRequired,
-  weatherArr: PropTypes.arrayOf(PropTypes.object).isRequired,
+  weatherArr: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 ConnectedMain.defaultProps = {
   sunset: {},
   sunrise: {},
-  isNight: undefined,
+  isNight: undefined
 };
 
-const Main = connect(mapStateToProps, mapDispatchToProps)(ConnectedMain);
+const Main = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConnectedMain);
 
 export default Main;
---
+++ src/components/weatherIcons.js
@@ -0,0 +1,84 @@
+const weatherIcons = {
+  chanceflurries: {
+    day: "wi wi-day-snow",
+    night: "wi wi-night-snow"
+  },
+  chancerain: {
+    day: "wi wi-day-rain",
+    night: "wi wi-night-rain"
+  },
+  chancesleet: {
+    day: "wi wi-day-sleet",
+    night: "wi wi-night-sleet"
+  },
+  chancesnow: {
+    day: "wi wi-day-snow",
+    night: "wi wi-night-snow"
+  },
+  chancetstorms: {
+    day: "wi wi-day-sprinkle",
+    night: "wi wi-night-sprinkle"
+  },
+  clear: {
+    day: "wi wi-day-sunny",
+    night: "wi wi-night-clear"
+  },
+  cloudy: {
+    day: "wi wi-cloudy",
+    night: "wi wi-cloudy"
+  },
+  flurries: {
+    day: "wi wi-day-snow",
+    night: "wi wi-night-snow"
+  },
+  fog: {
+    day: "wi wi-day-fog",
+    night: "wi wi-night-fog"
+  },
+  hazy: {
+    day: "wi wi-day-fog",
+    night: "wi wi-night-fog"
+  },
+  mostlycloudy: {
+    day: "wi wi-cloudy",
+    night: "wi wi-night-alt-cloudy"
+  },
+  mostlysunny: {
+    day: "wi wi-day-sunny-overcast",
+    night: "wi wi-night-alt-cloudy"
+  },
+  partlycloudy: {
+    day: "wi wi-day-cloudy",
+    night: "wi wi-night-alt-cloudy"
+  },
+  partlysunny: {
+    day: "wi wi-day-sunny-overcast",
+    night: "wi wi-night-alt-cloudy"
+  },
+  sleet: {
+    day: "wi wi-sleet",
+    night: "wi wi-sleet"
+  },
+  rain: {
+    day: "wi wi-rain",
+    night: "wi wi-rain"
+  },
+  snow: {
+    day: "wi wi-snow",
+    night: "wi wi-snow"
+  },
+  sunny: {
+    day: "wi wi-day-sunny",
+    night: "wi wi-night-clear"
+  },
+  tstorms: {
+    day: "wi wi-thunderstorm",
+    night: "wi wi-thunderstorm"
+  },
+  unknown: {
+    day: "wi wi-day-cloudy-high",
+    night: "wi wi-stars"
+  }
+};
+
+export default weatherIcons;
